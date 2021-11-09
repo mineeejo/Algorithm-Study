4b17c2e9180a343c82432da181f8e0483544f5ac
mineeejo <92152358+mineeejo@users.noreply.github.com>
2021-11-09 20:39:11 +0900

    Add files via upload

---
+++ 1109_문자열 압축.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def solution(x):
+    resultx = ''
+    minLen = len(x)
+
+    for num in range(1,len(x)+1):
+        i = 0
+        while i < len(x):
+            count = 1
+            while (i+num) < len(x) and x[i:i+num] == x[i+num:i+2*num]:
+                count += 1
+                i += num
+
+            if count == 1:
+                resultx += x[i:i+num]
+                i += num
+            else:
+                resultx += str(count)+x[i:i+num]
+                i += num
+
+        if minLen > len(resultx):
+            minLen = len(resultx)
+        resultx = ''
+
+    return minLen
+
